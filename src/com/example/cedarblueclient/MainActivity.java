c4fb2e5f8b78cb1b85fb014bbe63a6ca589789a3
Michael McRae <michaelmmcrae1@gmail.com>
2014-04-14 00:46:26 -0500

    First commit of client-side Android app mockup

---
+++ src/com/example/cedarblueclient/MainActivity.java
@@ -0,0 +1,114 @@
+package com.example.cedarblueclient;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.os.Build;
+
+public class MainActivity extends ActionBarActivity implements View.OnClickListener {
+	public Button submitButton = (Button) findViewById(R.id.button1);
+	public EditText topicEditText = (EditText) findViewById(R.id.editText2);
+	public EditText messageEditText = (EditText) findViewById(R.id.editText1);
+	
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
+        
+        submitButton.setOnClickListener(this);
+    
+    }
+    
+    @Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		String topicString = topicEditText.getText().toString();
+		String messageString = messageEditText.getText().toString();
+    	
+		HttpClient httpclient = new DefaultHttpClient();
+        HttpPost httppost = new HttpPost("http://cblue.local/wall_controller.php");
+
+        //httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+        try {
+        	List<NameValuePair> params = new ArrayList<NameValuePair>();
+    		params.add(new BasicNameValuePair("key", "value"));
+            httppost.setEntity(new UrlEncodedFormEntity(params));
+			HttpResponse response = httpclient.execute(httppost);
+		} catch (ClientProtocolException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	
+	}
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            return rootView;
+        }
+    }
+
+
+}
