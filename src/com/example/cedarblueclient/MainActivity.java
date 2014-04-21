32a436001b627d31cc0235ee0a70855836820ded
Michael McRae <michaelmmcrae1@gmail.com>
2014-04-20 22:47:35 -0500

    Add JSON Array retrieval

--- src/com/example/cedarblueclient/MainActivity.java
+++ src/com/example/cedarblueclient/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.cedarblueclient;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.zip.Inflater;
 
+import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
@@ -13,11 +18,17 @@ import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -26,89 +37,130 @@ import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 import android.os.Build;
 
-public class MainActivity extends ActionBarActivity implements View.OnClickListener {
-	public Button submitButton = (Button) findViewById(R.id.button1);
-	public EditText topicEditText = (EditText) findViewById(R.id.editText2);
-	public EditText messageEditText = (EditText) findViewById(R.id.editText1);
-	
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+@SuppressLint("NewApi") public class MainActivity extends ActionBarActivity implements
+		View.OnClickListener {
+	public Button submitButton;
+	public EditText topicEditText;
+	public EditText messageEditText;
 
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
-        }
-        
-        submitButton.setOnClickListener(this);
-    
-    }
-    
-    @Override
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		/*
+		 * Rather than create an AsyncTask to connect over HTTP, I just want to do it here
+		 * as a demo. This is a quick hack that allows the main activity to use HTTP
+		 */
+		if (android.os.Build.VERSION.SDK_INT > 9) {
+		      StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+		      StrictMode.setThreadPolicy(policy);
+		}
+
+		submitButton = (Button) findViewById(R.id.button1);
+		topicEditText = (EditText) findViewById(R.id.editText2);
+		messageEditText = (EditText) findViewById(R.id.editText1);
+
+		submitButton.setOnClickListener(this);
+		
+
+	}
+
+	/*public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		return inflater.inflate(R.layout.fragment, container, false);
+	}*/
+
+	@Override
 	public void onClick(View v) {
 		// TODO Auto-generated method stub
 		String topicString = topicEditText.getText().toString();
 		String messageString = messageEditText.getText().toString();
-    	
-		HttpClient httpclient = new DefaultHttpClient();
-        HttpPost httppost = new HttpPost("http://cblue.local/wall_controller.php");
 
-        //httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
-        try {
-        	List<NameValuePair> params = new ArrayList<NameValuePair>();
-    		params.add(new BasicNameValuePair("key", "value"));
-            httppost.setEntity(new UrlEncodedFormEntity(params));
+		List<NameValuePair> params = new ArrayList<NameValuePair>();
+		params.add(new BasicNameValuePair("topic", topicString));
+		params.add(new BasicNameValuePair("message", messageString));
+
+		HttpClient httpclient = new DefaultHttpClient();
+		HttpPost httppost = new HttpPost(
+				"http://192.168.0.183/cedarblue/wall_controller.php");
+		//192.168.56.1
+		//192.168.0.183
+		try {
+			httppost.setEntity(new UrlEncodedFormEntity(params));
 			HttpResponse response = httpclient.execute(httppost);
+
+			HttpEntity entity = response.getEntity();
+			InputStream is = entity.getContent();
+			BufferedReader reader = new BufferedReader(new InputStreamReader(is,"iso-8859-1"));
+			
+			StringBuilder sb = new StringBuilder();
+			String line = null;
+			while ((line = reader.readLine()) != null) {
+				sb.append(line + "\n");
+			}
+			is.close();
+			
+			String result = sb.toString();
+			JSONArray jArray = new JSONArray(result);
+			//JSONObject jObject = new JSONObject(result);
+			Toast.makeText(getApplicationContext(), "Success", Toast.LENGTH_LONG).show();
+			Log.e("log_tag", "JSON: " + jArray.toString());
+			//Toast.makeText(getApplicationContext(), "" + jObject.getString("message"), Toast.LENGTH_LONG).show();
+			Log.e("log_tag", "JSON at 1: " + jArray.get(1));
+			
+
 		} catch (ClientProtocolException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-    	
+
 	}
 
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
+	/**
+	 * A placeholder fragment containing a simple view.
+	 */
+	public static class PlaceholderFragment extends Fragment {
 
-        public PlaceholderFragment() {
-        }
+		public PlaceholderFragment() {
+		}
 
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
-        }
-    }
-
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+				Bundle savedInstanceState) {
+			View rootView = inflater.inflate(R.layout.fragment_main, container,
+					false);
+			return rootView;
+		}
+	}
 
 }
